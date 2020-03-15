ce10d89a180e2072f67250821bf9d56e4e14aebc
sam nejat <samnejat@sams-mbp.home>
2020-03-16 08:50:01 +1100

    post controller not working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,5 +2,23 @@ class CommentsController < ApplicationController
   before_action :check_for_login
 
   def new
+    @comment = Comment.new
   end
+
+  def create
+    @comment = Comment.new comment_params
+    comment.save
+    redirect_to post
+  end
+
+
+
+
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content, :img, :user_id)
+  end
+
+end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,41 @@ before_action :check_for_login
 
   def index
     @posts = Post.all
-    @user = User.all
+    @users = User.all
   end
 
   def new
+    @post = Post.new
   end
 
+  def create
+    @post = Post.new post_params
+    @users = User.all
+    post.save
+    redirect_to post
+  end
+
+  def edit
+    @post = Post.find params[:id]
+  end
+
+  def update
+    post = Post.find params[:id]
+    post.update post_params
+    redirect_to post
+  end
+
+  def show
+    @post = Post.find params[:id]
+    @comments = Comment.all
+  end
+
+
+
+
+  private
+  def post_params
+    params.require(:post).permit(:content, :img, :user_id)
+  end
 
 end
