77eb206be7144fa13e81c814a8f93c6b0d43cdda
sam nejat <samnejat@sams-mbp.home>
2020-03-18 09:17:27 +1100

    sign up from home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,22 @@
 class PagesController < ApplicationController
-  
+
   def home
+    @user = User.new
+    # redirect_to new_user_path
   end
+
+  # def new
+  #   @user = User.new
+  # end
+  #
+  # def create
+  #   @user = User.new user_params
+  #   if @user.save
+  #     session[:user_id] = @user.id
+  #     redirect_to posts_path
+  #   else
+  #     render :new
+  #   end
+  # end
+
 end
